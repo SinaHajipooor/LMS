d092355ec153f66487f706af80b515f6b2154a46
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-06-24 14:02:04 +0330

    add drawer to teacher dashbord

--- lib/screens/teacher/teacher_dashbord_screen.dart
+++ lib/screens/teacher/teacher_dashbord_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:lms/navigation/app_drawer.dart';
 import 'package:lms/navigation/bottom_tabas.dart';
-import 'package:lms/widgets/dashbord/calender.dart';
-import 'package:lms/widgets/dashbord/dashbord_info_cards.dart';
-import 'package:lms/widgets/elements/custom_appbar.dart';
-import 'package:lms/widgets/elements/spinner.dart';
+import 'package:lms/screens/profile/profile_screen.dart';
 
 class TeacherDashbordScreen extends StatefulWidget {
-  const TeacherDashbordScreen({super.key});
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  TeacherDashbordScreen({super.key});
 
   @override
   State<TeacherDashbordScreen> createState() => _TeacherDashbordScreenState();
@@ -15,7 +15,6 @@ class TeacherDashbordScreen extends StatefulWidget {
 
 class _TeacherDashbordScreenState extends State<TeacherDashbordScreen> {
   //----------------- state --------------------
-  bool _isLoading = false;
   final _scrollController = ScrollController();
   // ignore: unused_field
   bool _isFabVisible = true;
@@ -46,31 +45,51 @@ class _TeacherDashbordScreenState extends State<TeacherDashbordScreen> {
         return false;
       },
       child: Scaffold(
-        body: _isLoading
-            ? const Center(child: Spinner(size: 40))
-            : Stack(
-                children: [
-                  CustomScrollView(
-                    controller: _scrollController,
-                    slivers: [
-                      const CustomAppbar(title: 'داشبورد'),
-                      SliverList(
-                        delegate: SliverChildListDelegate.fixed(
-                          [
-                            const DashbordInfoCards(),
-                            const SizedBox(height: 15),
-                            const Divider(),
-                            const SizedBox(height: 10),
-                            PersianFullCalendar(
-                              key: UniqueKey(),
-                            )
-                          ],
+        key: widget._scaffoldKey,
+        appBar: AppBar(
+          elevation: 1,
+          backgroundColor: Colors.white,
+          leading: Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.menu, color: Colors.black),
+                onPressed: () => widget._scaffoldKey.currentState?.openDrawer(),
+              ),
+            ],
+          ),
+          actions: [
+            InkWell(
+              child: Container(
+                margin: const EdgeInsets.only(left: 12, top: 10),
+                child: Stack(
+                  children: [
+                    const CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/avatar.png'),
+                      radius: 18,
+                    ),
+                    Positioned(
+                      right: -1,
+                      top: 20,
+                      bottom: 1,
+                      child: Container(
+                        width: 11,
+                        height: 11,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color.fromARGB(255, 109, 223, 113),
                         ),
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                  ],
+                ),
               ),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed(ProfileScreen.routeName);
+              },
+            ),
+          ],
+        ),
+        drawer: const AppDrawer(),
       ),
     );
   }
