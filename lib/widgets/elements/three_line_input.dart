27a383fb3a3a3a92f32ed29e548326091d9050df
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-06-25 11:39:09 +0330

    seprate widget for description feilds

---
+++ lib/widgets/elements/three_line_input.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ThreeLineInput extends StatelessWidget {
+  final String value;
+  final String label;
+  final Function(String) onChanged;
+
+  const ThreeLineInput({
+    Key? key,
+    required this.value,
+    required this.label,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Text(
+            label,
+            style: const TextStyle(fontSize: 12),
+          ),
+        ),
+        Card(
+          elevation: 0.5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextFormField(
+              style: TextStyle(fontSize: 11),
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Line 1',
+              ),
+              onChanged: (value) => onChanged('$value\n${value.split('\n')[1]}\n${value.split('\n')[2]}'),
+              controller: TextEditingController(text: value.split('\n').length == 3 ? value : '$value\n\n'),
+              maxLines: 4,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
