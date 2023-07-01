7e002cf6b2ba13f1895cfb378fcedfd234b523b6
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-07-01 09:56:15 +0330

    update tms slider

--- lib/widgets/elements/slider/tms_slider.dart
+++ lib/widgets/elements/slider/tms_slider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:lms/widgets/elements/slider/tms_item.dart';
-// import '../spinner.dart';
+import 'package:lms/widgets/elements/spinner.dart';
 
 class TmsSlider extends StatefulWidget {
   // feilds
@@ -37,92 +36,65 @@ class _TmsSliderState extends State<TmsSlider> {
           ],
         ),
         const SizedBox(height: 30),
-        CarouselSlider.builder(
-          itemCount: widget.tmsList.length,
+        CarouselSlider(
+          carouselController: _controller,
           options: CarouselOptions(
-            scrollDirection: Axis.horizontal,
-            viewportFraction: 0.575,
-            aspectRatio: 2,
-            initialPage: 0,
-            disableCenter: true,
-            scrollPhysics: const BouncingScrollPhysics(),
-            enableInfiniteScroll: false,
-            enlargeCenterPage: true,
-            enlargeStrategy: CenterPageEnlargeStrategy.height,
+            height: 165,
+            autoPlay: false,
+            aspectRatio: 16 / 9,
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enableInfiniteScroll: true,
+            autoPlayAnimationDuration: const Duration(milliseconds: 600),
+            viewportFraction: 0.46,
           ),
-          itemBuilder: (context, index, realIndex) {
-            return TmsItem(imageUrl: widget.tmsList[realIndex]['logo'], title: widget.tmsList[realIndex]['name']);
-          },
+          items: widget.tmsList.map((item) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: AspectRatio(
+                          aspectRatio: 16 / 9,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: Image.network(
+                              item["logo"],
+                              fit: BoxFit.cover,
+                              loadingBuilder: (context, child, progress) {
+                                if (progress == null) return child;
+                                return const Center(
+                                  child: Spinner(size: 30),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                      child: Text(
+                        item['name'],
+                        style: const TextStyle(fontSize: 11.0, color: Colors.black),
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          }).toList(),
         ),
       ],
     );
   }
 }
-
-
-
-
-
-
-
-
-//  CarouselSlider(
-//           carouselController: _controller,
-//           options: CarouselOptions(
-//             height: 175,
-//             autoPlay: false,
-//             aspectRatio: 16 / 9,
-//             autoPlayCurve: Curves.fastOutSlowIn,
-//             enableInfiniteScroll: true,
-//             autoPlayAnimationDuration: const Duration(milliseconds: 600),
-//             viewportFraction: 0.46,
-//           ),
-//           items: widget.tmsList.map((item) {
-//             return Builder(
-//               builder: (BuildContext context) {
-//                 return Column(
-//                   children: [
-//                     Expanded(
-//                       child: Container(
-//                         width: MediaQuery.of(context).size.width * 0.8,
-//                         margin: const EdgeInsets.symmetric(horizontal: 8.0),
-//                         decoration: BoxDecoration(
-//                           borderRadius: BorderRadius.circular(12.0),
-//                         ),
-//                         child: AspectRatio(
-//                           aspectRatio: 16 / 9,
-//                           child: ClipRRect(
-//                             borderRadius: BorderRadius.circular(12.0),
-//                             child: Image.network(
-//                               item["logo"],
-//                               fit: BoxFit.cover,
-//                               loadingBuilder: (context, child, progress) {
-//                                 if (progress == null) return child;
-//                                 return Center(
-//                                   child: Spinner(size: 30),
-//                                 );
-//                               },
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     const SizedBox(height: 8.0),
-//                     Container(
-//                       child: Padding(
-//                         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-//                         child: Text(
-//                           item['name'],
-//                           style: const TextStyle(fontSize: 11.0, color: Colors.black),
-//                           maxLines: 2,
-//                           textAlign: TextAlign.center,
-//                           overflow: TextOverflow.ellipsis,
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 );
-//               },
-//             );
-//           }).toList(),
-//         ),
