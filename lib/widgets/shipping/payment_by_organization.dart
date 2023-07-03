792d2b5e63b792cf057d0f1efe559c022f9106d8
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-07-03 19:45:07 +0330

    add payment by organization

--- lib/widgets/shipping/payment_by_organization.dart
+++ lib/widgets/shipping/payment_by_organization.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 
 class PaymentByOrganization extends StatefulWidget {
@@ -8,6 +9,68 @@ class PaymentByOrganization extends StatefulWidget {
 }
 
 class _PaymentByOrganizationState extends State<PaymentByOrganization> {
+  void showSucessDialog() {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.success,
+      animType: AnimType.scale,
+      body: const SizedBox(
+        height: 100,
+        child: Center(
+          child: Text(
+            'درخواست شما با موفقیت ثبت گردید',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      btnOk: ElevatedButton(
+        onPressed: () => Navigator.of(context).pop(),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        ),
+        child: const Text('تایید'),
+      ),
+    ).show();
+  }
+
+  void showInputDialog() {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.warning,
+      animType: AnimType.scale,
+      body: const SizedBox(
+        height: 100,
+        child: Center(
+          child: Text(
+            'رویکرد پرداخت توسط دستگاه اجرایی را انتخاب می‌کنید ؟',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      btnOk: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+          showSucessDialog();
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        ),
+        child: const Text('تایید'),
+      ),
+      btnCancel: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+        ),
+        child: const Text('لغو'),
+      ),
+    ).show();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -19,7 +82,7 @@ class _PaymentByOrganizationState extends State<PaymentByOrganization> {
           image: AssetImage('assets/images/icons/office.png'),
         ),
       ),
-      onTap: () {},
+      onTap: () => showInputDialog(),
       title: const Text(
         'پرداخت توسط دستگاه اجرایی',
         style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
