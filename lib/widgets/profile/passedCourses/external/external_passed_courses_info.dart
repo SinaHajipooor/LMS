a9b2e945ddf7799ee9c0d3faa1f3e4a185f06993
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-07-12 14:04:31 +0330

    create external passed course

--- lib/widgets/profile/passedCourses/external/external_passed_courses_info.dart
+++ lib/widgets/profile/passedCourses/external/external_passed_courses_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lms/widgets/profile/passedCourses/external/external_passed_courses_modal.dart';
 
 class ExternalPassedCoursesInfo extends StatefulWidget {
   final List<dynamic> externalCourses;
@@ -9,13 +10,29 @@ class ExternalPassedCoursesInfo extends StatefulWidget {
 }
 
 class _ExternalPassedCoursesInfoState extends State<ExternalPassedCoursesInfo> {
+  _showJobinfoFormModal(BuildContext context, double deviceHeight) {
+    showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) {
+        return ExternalPassedCoursesModal(deviceHeight: deviceHeight);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       physics: const BouncingScrollPhysics(),
       child: Card(
-        elevation: 1,
+        elevation: 0,
         child: DataTable(
           dividerThickness: 0.5,
           horizontalMargin: 0,
@@ -29,9 +46,75 @@ class _ExternalPassedCoursesInfoState extends State<ExternalPassedCoursesInfo> {
             DataColumn(label: Center(child: Text('زمان پایان', style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)))),
             DataColumn(label: Center(child: Text('مدت', style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)))),
             DataColumn(label: Center(child: Text('عملیات', style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)))),
-            DataColumn(label: Center(child: Text('انتخاب', style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)))),
+            DataColumn(label: Center(child: Text('نمایش', style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)))),
           ],
-          rows: [],
+          rows: List<DataRow>.generate(
+            widget.externalCourses.length,
+            (index) => DataRow(
+              cells: [
+                DataCell(Center(child: Text(widget.externalCourses[index]['title'] ?? ''))),
+                DataCell(Center(child: Text(widget.externalCourses[index]['institute_title'] ?? ''))),
+                DataCell(Center(child: Text(widget.externalCourses[index]['address'] ?? ''))),
+                DataCell(Center(child: Text(widget.externalCourses[index]['start_date'] ?? ''))),
+                DataCell(Center(child: Text(widget.externalCourses[index]['end_date'] ?? ''))),
+                DataCell(Center(child: Text(widget.externalCourses[index]['duration'] ?? ''))),
+                DataCell(
+                  PopupMenuButton(
+                    icon: const Icon(Icons.more_vert, size: 19),
+                    elevation: 2,
+                    onSelected: (value) {
+                      print(value);
+                    },
+                    itemBuilder: (BuildContext context) => [
+                      PopupMenuItem(
+                          child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Wrap(
+                          spacing: 8,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.orange,
+                              radius: 15,
+                              child: IconButton(
+                                icon: const Icon(Icons.edit, color: Colors.white, size: 15),
+                                onPressed: () {
+                                  // Handle edit action
+                                },
+                              ),
+                            ),
+                            CircleAvatar(
+                              radius: 15,
+                              backgroundColor: Colors.red,
+                              child: IconButton(
+                                icon: const Icon(Icons.delete, color: Colors.white, size: 15),
+                                onPressed: () {
+                                  // Handle delete action
+                                },
+                              ),
+                            ),
+                            CircleAvatar(
+                              radius: 15,
+                              backgroundColor: Colors.blue,
+                              child: Center(
+                                child: IconButton(
+                                  icon: const Icon(Icons.file_copy_outlined, color: Colors.white, size: 15),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                    ],
+                  ),
+                ),
+                DataCell(IconButton(
+                  icon: const Icon(Icons.remove_red_eye, color: Colors.orange, size: 20),
+                  onPressed: () => _showJobinfoFormModal(context, MediaQuery.of(context).size.height),
+                )),
+              ],
+            ),
+          ),
         ),
       ),
     );
