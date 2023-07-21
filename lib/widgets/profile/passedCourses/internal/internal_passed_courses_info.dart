a8d68406970c4d555c37506e1a22301012624c77
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-07-21 20:37:45 +0330

    optimize

--- lib/widgets/profile/passedCourses/internal/internal_passed_courses_info.dart
+++ lib/widgets/profile/passedCourses/internal/internal_passed_courses_info.dart
@@ -8,13 +8,6 @@ class InternalPassedCoursesInfo extends StatefulWidget {
 }
 
 class _InternalPassedCoursesInfoState extends State<InternalPassedCoursesInfo> {
-  // bool _checkAll = false;
-  // List<Map<String, String>> _data = [
-  //   {'id': '001', 'device': 'Phone', 'operation': 'Call'},
-  //   {'id': '002', 'device': 'Laptop', 'operation': 'Code'},
-  //   {'id': '003', 'device': 'Tablet', 'operation': 'Play'},
-  //   {'id': '004', 'device': 'Desktop', 'operation': 'Design'},
-  // ];
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -38,79 +31,6 @@ class _InternalPassedCoursesInfoState extends State<InternalPassedCoursesInfo> {
             const DataColumn(label: Center(child: Text('وضعیت', style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)))),
           ],
           rows: [],
-          // rows: _data.map((item) {
-          //   return DataRow(
-          //     cells: [
-          //       DataCell(
-          //         Center(
-          //           child: Container(
-          //             decoration: BoxDecoration(
-          //               boxShadow: [
-          //                 BoxShadow(
-          //                   color: Colors.grey.withOpacity(0.2),
-          //                   spreadRadius: 2,
-          //                   blurRadius: 4,
-          //                   offset: const Offset(2, 0),
-          //                 ),
-          //               ],
-          //             ),
-          //             child: Text(item['id']!, style: const TextStyle(fontSize: 12)),
-          //           ),
-          //         ),
-          //       ),
-          //       DataCell(Center(child: Text(item['device']!, style: const TextStyle(fontSize: 12)))),
-          //       DataCell(Center(child: Text(item['device']!, style: const TextStyle(fontSize: 12)))),
-          //       DataCell(Center(child: Text(item['device']!, style: const TextStyle(fontSize: 12)))),
-          //       DataCell(
-          //         PopupMenuButton(
-          //           icon: const Icon(Icons.more_vert, size: 19),
-          //           elevation: 2,
-          //           onSelected: (value) {
-          //             print(value);
-          //           },
-          //           itemBuilder: (BuildContext context) => [
-          //             const PopupMenuItem(
-          //               child: SizedBox(
-          //                 width: 110,
-          //                 child: SingleChildScrollView(
-          //                   scrollDirection: Axis.horizontal,
-          //                   child: Row(
-          //                     children: [
-          //                       PopupMenuItem(
-          //                         value: 'edit',
-          //                         child: Icon(Icons.edit, color: Colors.orange, size: 20),
-          //                       ),
-          //                       PopupMenuItem(
-          //                         padding: EdgeInsets.only(left: 1),
-          //                         value: 'delete',
-          //                         child: Icon(Icons.delete, color: Colors.red, size: 20),
-          //                       ),
-          //                       PopupMenuItem(
-          //                         value: 'detail',
-          //                         child: Icon(Icons.file_copy_outlined, color: Colors.blue, size: 20),
-          //                       ),
-          //                     ],
-          //                   ),
-          //                 ),
-          //               ),
-          //             ),
-          //           ],
-          //         ),
-          //       ),
-          //       DataCell(
-          //         Checkbox(
-          //           value: item['checked'] == 'true',
-          //           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-          //           onChanged: (value) {
-          //             setState(() {
-          //               item['checked'] = value.toString();
-          //             });
-          //           },
-          //         ),
-          //       ),
-          //     ],
-          //   );
-          // }).toList(),
         ),
       ),
     );
