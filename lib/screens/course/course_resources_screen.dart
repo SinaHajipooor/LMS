bcfbb50c37b0fb5409139821c0360bc2c9613626
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-07-29 12:29:48 +0330

    place holder for course resources screen

--- lib/screens/course/course_resources_screen.dart
+++ lib/screens/course/course_resources_screen.dart
@@ -51,38 +51,47 @@ class _CourseResourcesScreenState extends State<CourseResourcesScreen> {
         ),
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              Container(
-                height: deviceSize.height * 0.35,
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 10),
-                child: Card(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                  elevation: 3,
-                  child: Stack(
-                    children: [
-                      Positioned.fill(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: const Directionality(
-                            textDirection: TextDirection.ltr,
-                            child: NetworkVideoPlayer(videoUrl: 'https://www.uplooder.net/f/tl/70/4cde8767d5e4e5ba6480165bc3437431/8.-Adding-Another-Piece-of-State.mp4'),
+        child: Visibility(
+          visible: widget.resources.isNotEmpty,
+          replacement: Center(
+            child: Text(
+              'محتوای آموزشی وجود ندارد !',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.normal),
+            ),
+          ),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Container(
+                  height: deviceSize.height * 0.35,
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 10),
+                  child: Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                    elevation: 3,
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: const Directionality(
+                              textDirection: TextDirection.ltr,
+                              child: NetworkVideoPlayer(videoUrl: 'https://www.uplooder.net/f/tl/70/4cde8767d5e4e5ba6480165bc3437431/8.-Adding-Another-Piece-of-State.mp4'),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Text(widget.courseName, style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
-              CourseResourcesList(
-                resources: widget.resources,
-                courseId: widget.courseId,
-              )
-            ],
+                Text(widget.courseName, style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+                CourseResourcesList(
+                  resources: widget.resources,
+                  courseId: widget.courseId,
+                )
+              ],
+            ),
           ),
         ),
       ),
