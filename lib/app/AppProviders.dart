6614946ff6b5c13269bd1361f3cb5aa4719f91e2
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-07-30 10:50:15 +0330

    add all providers config into seprate file

---
+++ lib/app/AppProviders.dart
@@ -0,0 +1,19 @@
+import 'package:provider/single_child_widget.dart';
+import 'AppImports.dart';
+
+final List<SingleChildWidget> appProviders = [
+  ChangeNotifierProvider(create: (_) => AuthProvider()),
+  ChangeNotifierProvider(create: (_) => IdentityProvider()),
+  ChangeNotifierProvider(create: (_) => JobProvider()),
+  ChangeNotifierProvider(create: (_) => InternalPassedCoursesProvider()),
+  ChangeNotifierProvider(create: (_) => ExternalPassedCoursesProvider()),
+  ChangeNotifierProvider(create: (_) => ActivityHistoryProvider()),
+  ChangeNotifierProvider(create: (_) => UniversityTeachingProvider()),
+  ChangeNotifierProvider(create: (_) => NonUniversityTeachingProvider()),
+  ChangeNotifierProvider(create: (_) => CompilationsProvider()),
+  ChangeNotifierProvider(create: (_) => LandingProvider()),
+  ChangeNotifierProvider(create: (_) => ElectronicCourseProvider()),
+  ChangeNotifierProvider(create: (_) => SimpleCourseProvider()),
+  ChangeNotifierProvider(create: (_) => TeachersPanelProvider()),
+  ChangeNotifierProvider(create: (_) => MyThemeModel()),
+];
